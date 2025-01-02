e3b14532e7e69645a8f0657dc4ec39b36b980168
Eduardo-Angeles <eduardo.jes.angeles.hdz@gmail.com>
2025-01-02 14:45:47 -0600

    Agregar configuración de sitio y corregir ruta del sitemap en robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), sitemap()]
+  site: "https://mrsirenescleaning.com/",
+  integrations: [tailwind(), preact(), sitemap()],
 });
